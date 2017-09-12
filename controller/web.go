872e27f181c77d1976d7582d1739a815e043fd74
Nickqiaoo <qyj970811@gmail.com>
2017-09-12 13:24:58 +0800

    实现多人访问

--- controller/web.go
+++ controller/web.go
@@ -9,20 +9,29 @@ import (
 	"net/http/cookiejar"
 	"os"
 	"strings"
+	"sync"
+	"net/url"
 )
 
-var url2 = "http://xk1.ahu.cn/CheckCode.aspx?"
-var jar, _ = cookiejar.New(nil)
-var c = &http.Client{
-	//CheckRedirect: func(req *http.Request, via []*http.Request) error {
-	//  return http.ErrUseLastResponse
-	// },
-	Jar: jar,
-}
+var mutex sync.Mutex
+var Url2 = "http://xk1.ahu.cn/CheckCode.aspx?"
+var Ma map[string] []*http.Cookie
 
 //  Home
 func Home(w http.ResponseWriter, r *http.Request) {
+	if Ma==nil{
+		Ma=make( map[string] []*http.Cookie)
+	}
+	jar,_:=cookiejar.New(nil)
+	u,_:=url.Parse(Url2)
+ 	c := http.Client{
+		//CheckRedirect: func(req *http.Request, via []*http.Request) error {
+		//  return http.ErrUseLastResponse
+		// },
+		Jar: jar,
+	}
 	fmt.Println("method", r.Method)
+	t, err := template.ParseFiles("view/result.html", "view/footer.html", "view/header.html")
 	if r.Method == "GET" {
 		if strings.HasPrefix(r.URL.Path, "/static") {
 			fmt.Println(r.URL.Path)
@@ -33,7 +42,12 @@ func Home(w http.ResponseWriter, r *http.Request) {
 					log.Fatal(err)
 				}
 				defer file1.Close()
-				req, err := c.Get(url2)
+				req, err := c.Get(Url2)
+				fmt.Println(c.Jar.Cookies(u))
+				cookie := http.Cookie{Name: "testcookiename", Value: "testcookievalue", Path: "/", MaxAge: 86400}
+				Ma[cookie.Name]=c.Jar.Cookies(u)
+				http.SetCookie(w, &cookie)
+				//c.Jar,_=cookiejar.New(nil)
 				image, _ := ioutil.ReadAll(req.Body)
 				file1.Write(image)
 				//var imagecode string
@@ -42,7 +56,6 @@ func Home(w http.ResponseWriter, r *http.Request) {
 			}
 			file := "static" + r.URL.Path[len("/static"):]
 			http.ServeFile(w, r, file)
-			return
 		}
 
 		t, err := template.ParseFiles("view/login.html", "view/footer.html", "view/header.html")
@@ -52,11 +65,19 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		t.ExecuteTemplate(w, "login", nil)
 	}
 	if r.Method == "POST" {
+		for k,v:= range(Ma){
+			fmt.Println(k,v)
+		}
+		c.Jar.SetCookies(u,Ma["testcookiename"])
 		r.ParseForm()
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
 		fmt.Println("yzm", r.Form["yzm"])
-		spider(r.Form["username"][0], r.Form["password"][0], r.Form["yzm"][0], c)
-		fmt.Fprintf(w, "Success")
+		result:=spider(r.Form["username"][0], r.Form["password"][0], r.Form["yzm"][0], &c )
+		if err != (nil) {
+			log.Fatal("template:", err)
+		}
+		t.ExecuteTemplate(w, "result", *result)
+		//fmt.Fprintf(w, "Success")
 	}
 }
