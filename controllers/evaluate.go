6e6036e6123cf9fea3c2c69a30527b3caaba07a5
Nickqiaoo <qyj970811@gmail.com>
2018-01-06 13:44:11 +0800

    修复无法提交

--- controllers/evaluate.go
+++ controllers/evaluate.go
@@ -63,7 +63,7 @@ func (c *MainController) Evaluate() {
 			fmt.Println("未找到课程列表")
 		}
 		course = append(course, "http://xk1.ahu.cn/"+ref)
-		fmt.Println(ref)
+		//fmt.Println(ref)
 	})
 	fmt.Println("课程数量：", len(course))
 
@@ -96,10 +96,6 @@ func (c *MainController) Evaluate() {
 
 		//获取教师数目
 		num := result.Find("#DataGrid1").Find("tbody").Find("tr.alt").Eq(0).Find("td").Length() - 2
-		/* Each(func(i int, s *goquery.Selection) {
-			num++
-		})
-		num-=1*/
 		fmt.Println("教师数目：", num)
 		fmt.Println(Url[35:64])
 		for k := 1; k <= num; k++ {
@@ -124,12 +120,8 @@ func (c *MainController) Evaluate() {
 		v.Add("txt1", "")
 		v.Add("TextBox1", "0")
 		v.Add("__EVENTVALIDATION", event)
-
-		if i == len(course)-1 {
-		v.Add("Button2", encoder.ConvertString(" 先完成评价，再提交 "))
-		} else {
 		v.Add("Button1", encoder.ConvertString("保 存"))
-		}
+
 		body := strings.NewReader(v.Encode())
 		req, err = http.NewRequest("POST", course[0], body)
 		checkErr(err)
@@ -144,6 +136,20 @@ func (c *MainController) Evaluate() {
 		checkErr(err)
 		fmt.Println("保存成功", response.Status)
 		fmt.Println()
+		if i == len(course)-1 {
+			v.Del("Button1")
+			v.Add("Button2", encoder.ConvertString(" 提  交 "))
+			body := strings.NewReader(v.Encode())
+			req, err = http.NewRequest("POST", Url, body)
+			checkErr(err)
+			req.Header.Add("Referer", Url)
+			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36")
+			response, err = client.Do(req)
+			checkErr(err)
+			fmt.Println("提交成功", response.Status)
+		}
+
 		if i == len(course)-1 {
 			res = "成功"
 		}
