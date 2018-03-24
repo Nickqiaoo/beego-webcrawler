bac30ba968011a2c30989a7c01f07781372e853c
Nickqiaoo <qyj970811@gmail.com>
2018-03-24 17:58:30 +0800

    修改部署脚本

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ cp -r github.com beego-webcrawler
 cp -r golang.org beego-webcrawler
 cd beego-webcrawler
 docker build -t beego-webcrawler:v1 .
+rm -rf github.com golang.org
 docker container stop $(docker container ls)
 docker run -d -p 80:80 -p  8088:8088  beego-webcrawler:v1 
