035cbc9b2ef4576e10d3c8700a4b30c6817c39b3
Nickqiaoo <nickqiaoo@gmail.com>
2019-03-30 19:13:06 +0800

    add redis,prometheus

--- controllers/default.go
+++ controllers/default.go
@@ -13,7 +13,30 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/astaxie/beego"
 	"github.com/axgle/mahonia"
+	"github.com/go-redis/redis"
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+var HTTPReqTotal *prometheus.CounterVec
+
+func init() {
+	HTTPReqTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Total number of HTTP requests made.",
+	}, []string{"path"})
+	prometheus.MustRegister(
+		HTTPReqTotal,
+	)
+}
+
+var redisclient = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+	Password: "", // no password set
+	DB:       0,  // use default DB
+})
+
+var encoder = mahonia.NewEncoder("gbk")
+var decoder = mahonia.NewDecoder("gbk")
 
 // Checkcodeurl 验证码url
 var Checkcodeurl = "http://jw3.ahu.cn/CheckCode.aspx"
@@ -24,11 +47,13 @@ type MainController struct {
 
 // Login 返回登陆界面
 func (c *MainController) Login() {
+	HTTPReqTotal.WithLabelValues("login").Inc()
 	c.TplName = "login.html"
 }
 
 // Checkcode 获取验证码
-func (c *MainController) Checkcode() {
+func (c *MainController) CheckCode() {
+	HTTPReqTotal.WithLabelValues("checkcode").Inc()
 	jar, _ := cookiejar.New(nil)
 	checkcodeurl, _ := url.Parse(Checkcodeurl)
 	client := http.Client{
@@ -51,6 +76,7 @@ func (c *MainController) Checkcode() {
 
 // Craw 登陆函数
 func (c *MainController) Craw() {
+	HTTPReqTotal.WithLabelValues("logincraw").Inc()
 	jar, _ := cookiejar.New(nil)
 	checkcodeurl, _ := url.Parse(Checkcodeurl)
 	client := http.Client{
@@ -77,8 +103,6 @@ func (c *MainController) Craw() {
 	//获取主页
 	url1 := "http://jw3.ahu.cn/default2.aspx"
 	v := url.Values{}
-	encoder := mahonia.NewEncoder("gbk")
-	decoder := mahonia.NewDecoder("gbk")
 	but := encoder.ConvertString("学生")
 	v.Add("__VIEWSTATE", "dDwxNTMxMDk5Mzc0Ozs+Z/4HcFs8ppzDjA3LEKdv34vgaQ4=")
 	v.Add("txtUserName", c.Ctx.Request.Form["username"][0])
@@ -128,28 +152,7 @@ func (c *MainController) Craw() {
 	c.TplName = "welcome.html"
 }
 
-// Querycredit 查询学分
-func (c *MainController) Querycredit() {
-	//初始化client
-	jar, _ := cookiejar.New(nil)
-	checkcodeurl, _ := url.Parse(Checkcodeurl)
-	client := http.Client{
-		Jar:     jar,
-		Timeout: time.Second * 10,
-	}
-	var err error
-	cookie := make([]*http.Cookie, 1)
-	cookie[0], err = c.Ctx.Request.Cookie("ASP.NET_SessionId")
-	if err != nil {
-		c.TplName = "fault.html"
-		return
-	}
-	client.Jar.SetCookies(checkcodeurl, cookie)
-	c.Ctx.Request.ParseForm()
-
-	//获取查询页
-	encoder := mahonia.NewEncoder("gbk")
-	decoder := mahonia.NewDecoder("gbk")
+func GetViewstate(c *MainController, client *http.Client) (view string, exits bool) {
 	cname := encoder.ConvertString(c.Ctx.Request.Form["name"][0])
 	resulturl := "http://jw3.ahu.cn/xscjcx.aspx?xh=" + c.Ctx.Request.Form["num"][0] + "&xm=" + url.QueryEscape(cname) + "&gnmkdm=N121605"
 	req, _ := http.NewRequest("GET", resulturl, nil)
@@ -169,14 +172,52 @@ func (c *MainController) Querycredit() {
 	//获取view，event
 	doc := decoder.NewReader(response.Body)
 	result, _ := goquery.NewDocumentFromReader(doc)
-	view, exits := result.Find("#Form1").Find("input").Eq(2).Attr("value")
+	view, exits = result.Find("#Form1").Find("input").Eq(2).Attr("value")
 	if !exits {
 		fmt.Println("__VIEWSTATE获取失败")
-		c.TplName = "toevaluate.html"
-		return
+		c.TplName = "fault.html"
+		return "", false
+	} else {
+		if err := redisclient.Set(c.Ctx.Request.Form["num"][0], view, 0).Err(); err != nil {
+			fmt.Println("redis写入失败")
+			c.TplName = "fault.html"
+			return "", false
+		}
+		return view, true
+	}
+}
 
+// Querycredit 查询学分
+func (c *MainController) QueryCredit() {
+	HTTPReqTotal.WithLabelValues("credit").Inc()
+	//初始化client
+	jar, _ := cookiejar.New(nil)
+	checkcodeurl, _ := url.Parse(Checkcodeurl)
+	client := &http.Client{
+		Jar:     jar,
+		Timeout: time.Second * 10,
+	}
+	var err error
+	cookie := make([]*http.Cookie, 1)
+	cookie[0], err = c.Ctx.Request.Cookie("ASP.NET_SessionId")
+	if err != nil {
+		c.TplName = "fault.html"
+		return
+	}
+	client.Jar.SetCookies(checkcodeurl, cookie)
+	c.Ctx.Request.ParseForm()
+
+	var view string
+	if view, err = redisclient.Get(c.Ctx.Request.Form["num"][0]).Result(); err != nil {
+		//获取查询页
+		var ok bool
+		if view, ok = GetViewstate(c, client); ok != true {
+			return
+		}
 	}
 	//event, _ := result.Find("#__EVENTVALIDATION").Attr("value")
+	cname := encoder.ConvertString(c.Ctx.Request.Form["name"][0])
+	resulturl := "http://jw3.ahu.cn/xscjcx.aspx?xh=" + c.Ctx.Request.Form["num"][0] + "&xm=" + url.QueryEscape(cname) + "&gnmkdm=N121605"
 	v := url.Values{}
 	v.Add("Button1", encoder.ConvertString("成绩统计"))
 	v.Add("__EVENTTARGET", "")
@@ -190,7 +231,7 @@ func (c *MainController) Querycredit() {
 
 	//构建新请求
 	body := strings.NewReader(v.Encode())
-	req, err = http.NewRequest("POST", resulturl, body)
+	req, err := http.NewRequest("POST", resulturl, body)
 	if err != nil {
 		log.Println(err)
 		c.TplName = "fault.html"
@@ -199,7 +240,7 @@ func (c *MainController) Querycredit() {
 	req.Header.Add("Referer", resulturl)
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36")
-	response, err = client.Do(req)
+	response, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
 		c.TplName = "fault.html"
@@ -207,7 +248,7 @@ func (c *MainController) Querycredit() {
 	}
 	log.Println(c.Ctx.Request.Form["num"][0], c.Ctx.Request.Form["name"][0], "学分结果页", response.Status)
 	ma, xf, jd := matchcredit(response)
-	c.Data["Name"] = decoder.ConvertString(cname)
+	c.Data["Name"] = c.Ctx.Request.Form["name"][0]
 	c.Data["Num"] = c.Ctx.Request.Form["num"][0]
 	c.Data["Xf"] = xf
 	c.Data["Jd"] = jd
@@ -216,11 +257,12 @@ func (c *MainController) Querycredit() {
 }
 
 // Querygrade 查询成绩
-func (c *MainController) Querygrade() {
+func (c *MainController) QueryGrade() {
+	HTTPReqTotal.WithLabelValues("grade").Inc()
 	//初始化client
 	jar, _ := cookiejar.New(nil)
 	checkcodeurl, _ := url.Parse(Checkcodeurl)
-	client := http.Client{
+	client := &http.Client{
 		Jar:     jar,
 		Timeout: time.Second * 10,
 	}
@@ -235,31 +277,13 @@ func (c *MainController) Querygrade() {
 	c.Ctx.Request.ParseForm()
 
 	//获取查询页
-	encoder := mahonia.NewEncoder("gbk")
-	decoder := mahonia.NewDecoder("gbk")
-	cname := encoder.ConvertString(c.Ctx.Request.Form["name"][0])
-	resulturl := "http://jw3.ahu.cn/xscjcx.aspx?xh=" + c.Ctx.Request.Form["num"][0] + "&xm=" + url.QueryEscape(cname) + "&gnmkdm=N121605"
-	req, _ := http.NewRequest("GET", resulturl, nil)
-	req.Header.Add("Referer", "http://jw3.ahu.cn/xs_main.aspx?xh="+c.Ctx.Request.Form["num"][0])
-	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36")
-	response, err := client.Do(req)
-	if err != nil {
-		c.TplName = "fault.html"
-		return
-	}
-	log.Println(c.Ctx.Request.Form["num"][0], "成绩查询页", response.Status)
-	if response.StatusCode != 200 {
-		c.TplName = "fault.html"
-		return
-	}
-
-	doc := decoder.NewReader(response.Body)
-	result, _ := goquery.NewDocumentFromReader(doc)
-	view, exits := result.Find("#Form1").Find("input").Eq(2).Attr("value")
-	if !exits {
-		fmt.Println("__VIEWSTATE获取失败")
-		c.TplName = "toevaluate.html"
-		return
+	var view string
+	if view, err = redisclient.Get(c.Ctx.Request.Form["num"][0]).Result(); err != nil {
+		//获取查询页
+		var ok bool
+		if view, ok = GetViewstate(c, client); ok != true {
+			return
+		}
 	}
 	//event, _ := result.Find("#__EVENTVALIDATION").Attr("value")
 	v := url.Values{}
@@ -274,8 +298,10 @@ func (c *MainController) Querygrade() {
 	//v.Add("__EVENTVALIDATION", event)
 
 	//构造新请求
+	cname := encoder.ConvertString(c.Ctx.Request.Form["name"][0])
+	resulturl := "http://jw3.ahu.cn/xscjcx.aspx?xh=" + c.Ctx.Request.Form["num"][0] + "&xm=" + url.QueryEscape(cname) + "&gnmkdm=N121605"
 	body := strings.NewReader(v.Encode())
-	req, err = http.NewRequest("POST", resulturl, body)
+	req, err := http.NewRequest("POST", resulturl, body)
 	if err != nil {
 		log.Println(err)
 		c.TplName = "fault.html"
@@ -284,7 +310,7 @@ func (c *MainController) Querygrade() {
 	req.Header.Add("Referer", resulturl)
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36")
-	response, err = client.Do(req)
+	response, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
 		c.TplName = "fault.html"
--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"beego-webcrawler/controllers"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/astaxie/beego"
 )
@@ -9,7 +10,9 @@ import (
 func init() {
 	beego.Router("/evaluate", &controllers.MainController{}, "post:Evaluate")
 	beego.Router("/login", &controllers.MainController{}, "get:Login;post:Craw")
-	beego.Router("/checkcode", &controllers.MainController{}, "get:Checkcode")
-	beego.Router("/grade", &controllers.MainController{}, "post:Querygrade")
-	beego.Router("/credit", &controllers.MainController{}, "post:Querycredit")
+	beego.Router("/checkcode", &controllers.MainController{}, "get:CheckCode")
+	beego.Router("/grade", &controllers.MainController{}, "post:QueryGrade")
+	beego.Router("/credit", &controllers.MainController{}, "post:QueryCredit")
+	beego.Handler("/metrics", promhttp.Handler())
+
 }
